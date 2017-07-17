f0e9d7b4d25d06d74c9e64b58abf08a467a4acda
Jeferson Araujo <Jeferson Araujo>
2017-07-16 23:43:51 -0300

    Add project files.

---
+++ Xamarin.ScanBar/Xamarin.ScanBar/HomePage.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Xamarin.ScanBar
+{
+    public class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+
+            Button scanBtn = new Button()
+            {
+                Text = "Escanear Código de Barras",
+                TextColor = Color.Black,
+                HorizontalOptions = LayoutOptions.FillAndExpand
+            };
+
+            scanBtn.Clicked += async (sender, args) =>
+            {
+                var scanResult = await Acr.BarCodes.BarCodes.Instance.Read();
+
+                if (!scanResult.Success)
+                {
+                    await this.DisplayAlert("Alerta!", "Erro! \n Falha ao ler o Código", "OK");
+                }
+                else
+                {
+                    await this.DisplayAlert("Scan realizado com sucesso!", String.Format("Formato do Cod. Barras: {0} \n Barcode Value: {1}", scanResult.Format, scanResult.Code), "OK");
+                }
+
+            };
+
+
+
+            Content = new StackLayout
+            {
+                BackgroundColor = Color.White,
+                Children = {
+                    scanBtn
+                }
+            };
+        }
+    }
+}
